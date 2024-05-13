520b5801059f9728945ab8797f47a27bcdd761b9
Валерий Манойлов <manojlovv@inbox.ru>
2024-05-14 02:08:47 +0300

    lol2

--- Config.py
+++ Config.py
@@ -1,9 +1,9 @@
 class Config:
     BOT_TOKEN = "6887073782:AAF6Ro9yeg4V0YjF-CCgBv_USVdtNfJqOiI"
     Users = {
-        "@Nastya_Nastya_Nastya":"НАСТЯ ПОРА БОТАТЬ!!!",
-        "@ShineBS":"СЕРЕЖА ПОРА БОТАТЬ!!!",
-        "@Satlykovs":"САНДЖАР ПОРА БОТАТЬ!!!",
-        "@Super_roAy6b":"МАКСИМ ПОРА БОТАТЬ!!!",
-        "@ValeriyManoilov":"ВАЛЕРА ПОРА БОТАТЬ!!!"
+        "@Nastya_Nastya_Nastya":"Настя, пора баиньки)",
+        "@ShineBS":"Сережа, пора баиньки)",
+        "@Satlykovs":"Санджар, пора баиньки)",
+        "@Super_roAy6b":"Максим, пора баиньки)",
+        "@ValeriyManoilov":"Валера, пора баиньки)"
     }
